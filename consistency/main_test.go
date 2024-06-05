b04f17c4e46cac732206544c4df4fe0a8dfeb9ea
jgough <joe.gough@datatrails.ai>
2024-06-05 11:26:10 +0100

    Make a consistency demo re: AB#9491

--- consistency/main_test.go
+++ consistency/main_test.go
@@ -1,11 +1,16 @@
-package consistency
+package main
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(t *testing.T) {
-	require.True(t, true)
+
+	verified, err := ConsistencyDemo()
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, verified)
+
 }
